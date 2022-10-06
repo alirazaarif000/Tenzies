0e81dc2e4c925363bd65fa17745939156da26d3d
Ali Raza <alirazaarif000@gmail.com>
2022-10-06 23:01:09 +0500

    First Tenzies Commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React,{useState, useEffect} from "react";
+import Die from "./components/die"
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
+export default function App(){
+
+    const [diceFunc, setDiceFunc]= useState(allNewDice())
+    const [tenzies, setTenzies] =useState(false)
+    const [countRoll, setCountRoll]= useState(0)
+
+    
+    
+    useEffect(()=>{
+        const allHeld= diceFunc.every(die=> die.isHeld)
+        const firstValue= diceFunc[0].value
+        const allSameValue= diceFunc.every(die=> firstValue===die.value)
+        if(allHeld && allSameValue){
+            setTenzies(true)
+        }    
+    }, [diceFunc])
+    function generateDice(){
+        return {
+            value:Math.ceil(Math.random() * 6), 
+            isHeld:false,
+            id:nanoid()
+    }
+    }
+    function allNewDice(){
+        let newDice= []
+        for(let i=0; i<10; i++){
+            newDice.push(generateDice())
+        }
+
+        return newDice
+    }
+    function diceRoll(){
+        setDiceFunc(prevDiceFunc=>prevDiceFunc.map(dice=>{
+           return dice.isHeld ? dice : generateDice()
+        }))
+        setCountRoll(countRoll+1
+        )
+    }
+    function newDiceRoll(){
+        setTenzies(false)
+        setDiceFunc(allNewDice)
+        setCountRoll(0)
+    }
+    function holdDice(id){
+        setDiceFunc(prevDiceFunc=> prevDiceFunc.map(die=>{
+            return die.id===id ? {...die, isHeld: !die.isHeld} : die
+        })
+        )
+    }
+    
+    const diceElement= diceFunc.map(die=> <Die 
+        value= {die.value} 
+        isHeld={die.isHeld}
+        key = {die.id}
+        holdDice= {()=>holdDice(die.id)}
+        />)
+    
+    
+    return(
+        <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="container">
+                {diceElement}
+            </div>
+            <button className="btn-roll" onClick={tenzies ? newDiceRoll : diceRoll}>{tenzies? "New Game" : "Roll"}</button>
+            <h3>Total Rolls: {countRoll}</h3>
+            {tenzies && <Confetti />}
+        </main>
+    )
+}
---
+++ src/components/die.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "#ffffff"
+    }
+    function dot() {
+        const dotElement=[]
+        const spanElement= <span className="dot1"></span>
+        if (props.value === 1) {
+            return (
+                <div className="die" style={styles} onClick={props.holdDice}>
+                    <div className="die-dots">
+                        <span className="dot1"></span>
+                    </div>
+                </div>)
+        }
+        else if (props.value === 2) {
+            return (
+                <div className="die" style={styles} onClick={props.holdDice}>
+                    <div className="die-dots">
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                    </div>
+                </div>)
+        }
+        else if (props.value === 3) {
+            return (
+                <div className="die" style={styles} onClick={props.holdDice}>
+                    <div className="die-dots">
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                    </div>
+                </div>)
+        }
+        else if (props.value === 4) {
+            return (
+                <div className="die" style={styles} onClick={props.holdDice}>
+                    <div className="die-dots">
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                    </div>
+                </div>)
+        }
+        else if (props.value === 5) {
+            return (
+                <div className="die" style={styles} onClick={props.holdDice}>
+                    <div className="die-dots">
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                    </div>
+                </div>)
+        }
+        else if (props.value === 6) {
+            return (
+                <div className="die" style={styles} onClick={props.holdDice}>
+                    <div className="die-dots">
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                        <span className="dot1"></span>
+                    </div>
+                </div>)
+        }
+    }
+    return (
+        dot()
+    
+    )
+}
